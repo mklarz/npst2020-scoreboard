6d039d92f59a2fdec6e336cc13dad86e3e9fdfe7
mklarz <mklarz@protonmail.com>
2020-12-10 10:04:23 +0000

    Add initial scoreboard

---
+++ check_scoreboard.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+## NOTICE
+# Dirty quick script to track the NPST2020 scoreboard. Buy me a beer :))
+
+## CONSTANTS
+SCOREBOARD_URL="https://dass.npst.no/.netlify/functions/scoreboard"
+SCRIPT_PATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+## GOGO
+echo "Starting script..."
+cd $SCRIPT_PATH # dirty
+
+# Download newest scoreboard
+wget -q "$SCOREBOARD_URL" -O "$SCRIPT_PATH/scoreboard.min.json"
+# Let's "prettify" the json as well
+cat "$SCRIPT_PATH/scoreboard.min.json" | jq > "$SCRIPT_PATH/scoreboard.json"
+
+if [[ `git status --porcelain` ]]; then
+	echo "There are differences, updating"
+	#git add -A
+	#git commit -m "[SCPREBOARD] update"
+	#git push origin main
+else
+	echo "No differences"
+fi
